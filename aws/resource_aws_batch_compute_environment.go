d62c3b5c4ea37ef3f262be49f4f292aefe6273c4
shibataka000 <chise.alter.pasta@gmail.com>
2017-09-11 21:51:17 +0900

    Use validateArn function. After run test at once, I will delete test case about arn.

--- aws/resource_aws_batch_compute_environment.go
+++ aws/resource_aws_batch_compute_environment.go
@@ -18,8 +18,6 @@ const (
 )
 
 var reComputeEnvironmentName = regexp.MustCompile(`^[A-Za-z0-9_]*$`)
-var reArnOfIamRole = regexp.MustCompile(`^arn:aws:iam::[0-9]{12}:role/.*$`)
-var reArnOfIamInstanceProfile = regexp.MustCompile(`^arn:aws:iam::[0-9]{12}:instance-profile/.*$`)
 
 func resourceAwsBatchComputeEnvironment() *schema.Resource {
 	return &schema.Resource{
@@ -65,7 +63,7 @@ func resourceAwsBatchComputeEnvironment() *schema.Resource {
 							Type:         schema.TypeString,
 							Required:     true,
 							ForceNew:     true,
-							ValidateFunc: isArnOfIamInstanceProfile,
+							ValidateFunc: validateArn,
 						},
 						"instance_type": {
 							Type:     schema.TypeSet,
@@ -91,7 +89,7 @@ func resourceAwsBatchComputeEnvironment() *schema.Resource {
 							Type:         schema.TypeString,
 							Optional:     true,
 							ForceNew:     true,
-							ValidateFunc: isArnOfIamRole,
+							ValidateFunc: validateArn,
 						},
 						"subnets": {
 							Type:     schema.TypeSet,
@@ -112,7 +110,7 @@ func resourceAwsBatchComputeEnvironment() *schema.Resource {
 			"service_role": {
 				Type:         schema.TypeString,
 				Required:     true,
-				ValidateFunc: isArnOfIamRole,
+				ValidateFunc: validateArn,
 			},
 			"state": {
 				Type:         schema.TypeString,
@@ -447,38 +445,6 @@ func isCorrentComputeEnvironmentName(i interface{}, k string) (s []string, es []
 	if !(reComputeEnvironmentName.MatchString(v) && len(v) <= 128) {
 		es = append(es, fmt.Errorf("computeEnvironmentName must be up to 128 letters (uppercase and lowercase), numbers, and underscores."))
 		return
-	}
-
-	return
-}
-
-func isArnOfIamRole(i interface{}, k string) (s []string, es []error) {
-	v, ok := i.(string)
-	if !ok {
-		es = append(es, fmt.Errorf("expected type of %s to be string", k))
-		return
-	}
-
-	if !reArnOfIamRole.MatchString(v) {
-		es = append(es, fmt.Errorf("expected arn of iam role, got %s", v))
-		return
-
-	}
-
-	return
-}
-
-func isArnOfIamInstanceProfile(i interface{}, k string) (s []string, es []error) {
-	v, ok := i.(string)
-	if !ok {
-		es = append(es, fmt.Errorf("expected type of %s to be string", k))
-		return
-	}
-
-	if !reArnOfIamInstanceProfile.MatchString(v) {
-		es = append(es, fmt.Errorf("expected arn of iam instance profile, got %s", v))
-		return
-
 	}
 
 	return
